6e98b7e1563255856077414f66cbb8384c6b8e17
zhangping <2425238116@qq.com>
2022-04-23 17:09:57 +0800

    test:测试2

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <Link to="/form">form</Link>: form表单 <br />
       <Link to="/searchFrom">searchFrom</Link>: 筛选条件 (公共组件), table, 分页 <br />
       <Link to="/parentToChildFn">parentToChildFn</Link>: 父组件调用子组件方法 <br />
-      test1
+      test2
     </div>
   )
 }
